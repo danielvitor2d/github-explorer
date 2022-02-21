48f171e06291bd630fdf7c8ad9705b22e72ddd5c
danielvitor2d <ripsea.rpg321@gmail.com>
2022-02-21 10:29:08 -0300

    Remove dotenv from server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const path = require('path')
-const dotEnv = require('dotenv')
 
 const app = express()
-
-dotEnv.config()
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.set('port', process.env.PORT || 3000)
